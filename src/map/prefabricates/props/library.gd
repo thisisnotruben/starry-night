2f0f540da6c79975b129b66cabb34799d7044ee4
Ruben Alvarez Reyes <ruben.alvarezreyes@gm.com>
2023-01-18 20:12:48 -0600

    map/unit work

---
+++ src/map/prefabricates/props/library.gd
@@ -0,0 +1,16 @@
+extends Spatial
+
+
+func _ready():
+	for child in get_children():
+		var mesh = child.get_child(0).get_child(0)
+		if mesh is MeshInstance:
+			mesh.get_parent().remove_child(mesh)
+			add_child(mesh)
+			mesh.owner = self
+		else:
+			printerr(child, "-- didn't find mesh")
+	
+	var packed_scene = PackedScene.new()
+	packed_scene.pack(self)
+	ResourceSaver.save(filename.get_basename() + "_mesh.tscn", packed_scene)
